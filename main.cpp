c526635917ffc75b0731b36ca52d457f5af93b5a
dmitry <sda97g@gmail.com>
2018-10-24 18:37:30 +0500

    Added read-only properties support

--- main.cpp
+++ main.cpp
@@ -5,11 +5,18 @@
 template <typename T>
 class Property {
 public:
+	class CallSetterOfReadOnlyPropertyException : public std::logic_error {
+	public:
+		CallSetterOfReadOnlyPropertyException() :
+			std::logic_error("Attempt to set value for read only property") {}
+	};
+
 	using Getter = std::function<T(const T&)>;
 	using Setter = std::function<void(const T&, T&)>;
 
 	static Getter DEFAULT_GETTER;
 	static Setter DEFAULT_SETTER;
+	static Setter READ_ONLY_SETTER;
 
 	Property(Getter getter = nullptr, Setter setter = nullptr) {
 		m_getter = (getter == nullptr ? DEFAULT_GETTER : getter);
@@ -22,6 +29,27 @@ public:
 						   std::bind(setter, obj, std::placeholders::_1));
 	}
 
+	static Property<T> readOnly(Getter getter) {
+		return Property<T>(getter, READ_ONLY_SETTER);
+	}
+
+	template <typename C>
+	static Property<T> readOnlyOf(C* obj, T(C::*getter)()const) {
+		return Property<T>(std::bind(getter, obj), READ_ONLY_SETTER);
+	}
+
+	static Property<T> readOnlyOf(T constant) {
+		return readOnly([=](const T&) {
+			return constant;
+		});
+	}
+
+	static Property<T> readOnlyOf(T(*getter)()) {
+		return readOnly([=](const T&) {
+			return getter();
+		});
+	}
+
 	T get() const {
 		return m_getter(m_value);
 	}
@@ -30,12 +58,12 @@ public:
 		m_setter(value, m_value);
 	}
 
-	void operator =(const T& value) {
-		set(value);
+	operator T() {
+		return m_getter(m_value);
 	}
 
-	operator T() {
-		return get();
+	void operator =(const T& value) {
+		m_setter(value, m_value);
 	}
 
 private:
@@ -54,6 +82,13 @@ typename Property<T>::Setter Property<T>::DEFAULT_SETTER = [](const T& value, T&
 	field = value;
 };
 
+template <typename T>
+typename Property<T>::Setter Property<T>::READ_ONLY_SETTER = [](const T& value, T& field) -> void {
+	(void)value;
+	(void)field;
+	throw CallSetterOfReadOnlyPropertyException();
+};
+
 // TEST /////////////////////////////////////////
 
 #include <QCoreApplication>
@@ -61,21 +96,28 @@ typename Property<T>::Setter Property<T>::DEFAULT_SETTER = [](const T& value, T&
 
 class Test {
 public:
+	Property<int> SimpleIntProp;
+	Property<QString> SimpleStrProp;
+
 	Property<int> IntProp;
 	Property<QString> StrProp;
+
+	Property<int> ReadOnlyIntProp;
+	Property<QString> ReadOnlyStrProp;
+	Property<QStringList> ReadOnlyStringsProp;
+	Property<QList<int>> ReadOnlyIntsProp;
 
 	Test() :
 		m_v(-1) {
 		IntProp = Property<int>::of(this, &Test::v, &Test::setV);
-//		IntProp = Property<int>(
-//		[&](const int& field) {
-//			qDebug() << "getter";
-//			return m_v;
-//		},
-//		[&](const int& value, int& field) {
-//			qDebug() << "setter";
-//			m_v = value;
-//		});
+		StrProp = Property<QString>::of(this, &Test::str, &Test::setStr);
+		ReadOnlyIntProp = Property<int>::readOnly([](const int& field) {
+			Q_UNUSED(field)
+			return 42;
+		});
+		ReadOnlyStrProp = Property<QString>::readOnlyOf("Hello World!");
+		ReadOnlyStringsProp = Property<QStringList>::readOnlyOf(this, &Test::strings);
+		ReadOnlyIntsProp = Property<QList<int>>::readOnlyOf(&Test::ints);
 	}
 
 private:
@@ -88,21 +130,58 @@ private:
 		m_v = v;
 	}
 
+	QString str() const {
+		qDebug() << "getter str";
+		return m_str;
+	}
+
+	void setStr(const QString& str) {
+		qDebug() << "setter str";
+		m_str = str;
+	}
+
+	QStringList strings() const {
+		static QStringList strs = {
+			"Hello",
+			"World",
+			"!"
+		};
+		return strs;
+	}
+
+	static QList<int> ints() {
+		static QList<int> intsList = {3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5};
+		return intsList;
+	}
+
 	int m_v;
+	QString m_str;
 };
 
 int main(int argc, char *argv[]) {
-	QCoreApplication a(argc, argv);
+	QCoreApplication app(argc, argv);
 
 	Test t;
 
-	t.IntProp = 10;
-	t.StrProp = "Hello!";
+	t.SimpleIntProp = 10;
+	t.IntProp = 20;
+	t.SimpleStrProp = "Hello";
+	t.StrProp = "World!";
+//	t.ReadOnlyIntProp = 30; // cases exception
+//	t.ReadOnlyStrProp = "Goodbye World!"; // cases exception
+//	t.ReadOnlyStringsProp = {"Hello", "World!"}; // cases exception
+//	t.ReadOnlyIntsProp = {1, 2, 3}; // cases exception
 
-	int x = t.IntProp;
-	QString str = t.StrProp;
+	int a = t.SimpleIntProp;
+	int b = t.IntProp;
+	QString c = t.SimpleStrProp;
+	QString d = t.StrProp;
+	int e = t.ReadOnlyIntProp;
+	QString f = t.ReadOnlyStrProp;
+	QStringList g = t.ReadOnlyStringsProp;
+	QList<int> h = t.ReadOnlyIntsProp;
 
-	qDebug() << x << str;
+	qDebug() << a << b << c << d << e << f << g << h;
 
-	return a.exec();
+	return app.exec();
 }
